f5e93bf3741e9378776c3f6689b9a8c0aa0f2375
K-Gamma <ganma.kato@gmail.com>
2025-07-02 22:03:33 +0900

    Decorative icon should have empty alt when aria-hidden is present
    https://github.com/K-Gamma/kakebo/pull/1#discussion_r2179975919

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,13 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/file.svg" alt="" width={16} height={16} />
           Learn
         </a>
         <a
@@ -73,13 +67,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/window.svg" alt="" width={16} height={16} />
           Examples
         </a>
         <a
@@ -88,13 +76,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/globe.svg" alt="" width={16} height={16} />
           Go to nextjs.org →
         </a>
       </footer>
